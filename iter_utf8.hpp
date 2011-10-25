4dd6b03415bcb5e2283f06a98958add4ddd0af5e
Zeev Tarantov <zeev.tarantov@gmail.com>
2011-10-25 15:52:51 +0200

    optimization

--- iter_utf8.hpp
+++ iter_utf8.hpp
@@ -19,12 +19,6 @@ private:
 };
 
 /* TODO:
- * Instead of pointer to container, just store the pointer to end (-4).
- * In dereference operator, don't check whether payload bytes can be read:
- *   if we're past the end pointer by less than 4:
- *     clear a 4 byte buffer
- *     copy needed bytes into buffer
- *     parse data in buffer
  * In case of two payload bytes, ifdef unaligned check of: *p & 0xc0c0 == 0x8080
  * Add check code point is in minimal length encoding.
  */
@@ -32,18 +26,18 @@ class utf8_iterator: public std::iterator<std::forward_iterator_tag, int32_t>
 {
   friend class UTF8;
 private:
-  const UTF8 *utf8_;
   const uint8_t *p_;
+  const uint8_t *end_;
   int curr_seq_len_;
-  utf8_iterator(const UTF8 *utf8, const uint8_t *p) : utf8_(utf8), p_(p) { curr_seq_len_ = 0; }
+  utf8_iterator(const uint8_t *p, const uint8_t *end) : p_(p), end_(end), curr_seq_len_(0) { }
 public:
-  utf8_iterator(const utf8_iterator& other) { utf8_ = other.utf8_; p_ = other.p_; }
+//  utf8_iterator(const utf8_iterator& other) { utf8_ = other.utf8_; p_ = other.p_; }
   utf8_iterator& operator++();
   bool operator!=(const utf8_iterator& other) { return p_ != other.p_; }
   int32_t operator*();
 };
 
-inline utf8_iterator UTF8::begin() const { return utf8_iterator(this, s_); }
-inline utf8_iterator UTF8::end() const { return utf8_iterator(this, end_); }
+inline utf8_iterator UTF8::begin() const { return utf8_iterator(s_, end_); }
+inline utf8_iterator UTF8::end() const { return utf8_iterator(end_, end_); }
 
 #endif /* defined(ITER_UTF8_HPP_) */
