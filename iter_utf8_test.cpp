b439d16b64699ce58af637ba26aaf3693173481d
Zeev Tarantov <zeev.tarantov@gmail.com>
2011-10-27 05:09:28 +0200

    fix tests

--- iter_utf8_test.cpp
+++ iter_utf8_test.cpp
@@ -1,39 +1,18 @@
 #include <cstdio>
-#include <cstring>
-//#include "iter_utf8.hpp"
 #include "utf8_foreach_codepoint.hpp"
 
 int main()
 {
-  const char *test = "日本語テスト彳";
-  utf8_foreach_codepoint(test, strlen(test), [](int32_t u) { printf("%06X ", u); });
-  puts("");
-/*
   {
     const char *test = "日本語テスト彳";
-    UTF8 utf8(test, strlen(test));
-    for (auto i = utf8.begin(); i != utf8.end(); ++i)
-      printf("%06X ", *i);
+    utf8_foreach_codepoint(test, strlen(test), [](int32_t u) { printf("%06X ", u); });
     puts("");
   }
   {
     const char *test = "abcd\xE0";
-    UTF8 utf8(test, strlen(test));
     int n = 0;
-    for (auto i = utf8.begin(); i != utf8.end(); ++i)
-      ++n;
+    utf8_foreach_codepoint(test, strlen(test), [&n](int32_t u) { printf("%06X ", u); ++n; });
     printf("%d\n", n);
   }
-  {
-    const char *test = "abcd\xE0";
-    UTF8 utf8(test, strlen(test));
-    int n = 0;
-    for (auto i = utf8.begin(); i != utf8.end(); ++i) {
-      printf("%06X ", *i);
-      ++n;
-    }
-    printf("%d\n", n);
-  }
-*/
   return 0;
 }
